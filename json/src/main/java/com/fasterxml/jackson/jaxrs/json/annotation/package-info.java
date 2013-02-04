f7af190fc69abe4d0936e8cd342595cc95e6920b
Tatu Saloranta <tsaloranta@gmail.com>
2013-02-04 15:34:05 -0800

    javadoc changes

--- json/src/main/java/com/fasterxml/jackson/jaxrs/json/annotation/package-info.java
+++ json/src/main/java/com/fasterxml/jackson/jaxrs/json/annotation/package-info.java
@@ -1,5 +1,9 @@
 /**
  * Package that contains utility classes and methods for
  * the JAX-RS JSON provider module.
+ *<p>
+ * NOTE: starting with version 2.2, general-purpose annotations
+ * will be moved to a shared package, and this package will only
+ * contains JSON-specific annotations.
  */
 package com.fasterxml.jackson.jaxrs.json.annotation;
