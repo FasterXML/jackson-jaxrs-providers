f8bd9a6d4a457ec77ac6f152a2c1aaffa3598b8a
M. Tarik Yurt <mtyurt@gmail.com>
2015-06-17 09:59:30 +0300

    Remove deprecated annotation config

--- yaml/src/main/java/com/fasterxml/jackson/jaxrs/yaml/YAMLEndpointConfig.java
+++ yaml/src/main/java/com/fasterxml/jackson/jaxrs/yaml/YAMLEndpointConfig.java
@@ -37,25 +37,6 @@ public class YAMLEndpointConfig
                 ;
     }
 
-    @SuppressWarnings("deprecation")
-    @Override
-    protected void addAnnotation(Class<? extends Annotation> type,
-                                 Annotation annotation, boolean forWriting) {
-        if (type == com.fasterxml.jackson.jaxrs.yaml.annotation.JacksonFeatures.class) {
-            com.fasterxml.jackson.jaxrs.yaml.annotation.JacksonFeatures feats =
-                    (com.fasterxml.jackson.jaxrs.yaml.annotation.JacksonFeatures) annotation;
-            if (forWriting) {
-                _serEnable = nullIfEmpty(feats.serializationEnable());
-                _serDisable = nullIfEmpty(feats.serializationDisable());
-            } else {
-                _deserEnable = nullIfEmpty(feats.deserializationEnable());
-                _deserDisable = nullIfEmpty(feats.deserializationDisable());
-            }
-        } else {
-            super.addAnnotation(type, annotation, forWriting);
-        }
-    }
-
     @Override
     public Object modifyBeforeWrite(Object value) {
         // nothing to add
