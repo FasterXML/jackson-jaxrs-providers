de9de7fa545b1130992429d8a596cd764b64dd4c
Cowtowncoder <tatu.saloranta@iki.fi>
2015-10-12 13:55:19 -0700

    prepare for 2.6.3

--- smile/src/main/java/com/fasterxml/jackson/jaxrs/smile/SmileMediaTypes.java
+++ smile/src/main/java/com/fasterxml/jackson/jaxrs/smile/SmileMediaTypes.java
@@ -3,7 +3,6 @@ package com.fasterxml.jackson.jaxrs.smile;
 import javax.ws.rs.core.MediaType;
 
 public class SmileMediaTypes {
-
 	public static final String    APPLICATION_JACKSON_SMILE      = "application/x-jackson-smile";
 	public static final MediaType APPLICATION_JACKSON_SMILE_TYPE = MediaType.valueOf(APPLICATION_JACKSON_SMILE);
 }
