565c97a9bdd2afcc50295bec34b523943a6f50c2
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-07-05 20:31:19 -0700

    prepare for 2.8.0 releae

--- base/src/main/java/com/fasterxml/jackson/jaxrs/base/JsonParseExceptionMapper.java
+++ base/src/main/java/com/fasterxml/jackson/jaxrs/base/JsonParseExceptionMapper.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonParseException;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
-import javax.ws.rs.ext.Provider;
 
 /**
  * Implementation of {@link ExceptionMapper} to send down a "400 Bad Request"
