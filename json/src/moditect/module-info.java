cd7788466e77c4ec11239813fe07e2ef3c516687
GedMarc <ged_marc@hotmail.com>
2020-11-02 02:32:04 +0200

    Module-Info Enhancements - JAX-RS updates for Jakarta Release version
    Zero impact on backwards compatibility
    https://github.com/FasterXML/jackson-databind/issues/2910

--- json/src/moditect/module-info.java
+++ json/src/moditect/module-info.java
@@ -12,8 +12,15 @@ module com.fasterxml.jackson.jaxrs.json {
 
     requires com.fasterxml.jackson.jaxrs.base;
 
+    //oracle location
     requires static javax.ws.rs.api;
+    //oracle location
     requires static java.ws.rs;
+    //jakarta initial location - 2.x
+    requires static javax.ws.rs;
+    //jakarta 3.x final location - https://github.com/jboss/jboss-jakarta-jaxrs-api_spec
+    requires static jakarta.ws.rs;
+    //jakarta 3.x final location - https://github.com/eclipse-ee4j/jaxrs-api
     requires static jakarta.ws.rs.api;
 
     provides javax.ws.rs.ext.MessageBodyReader with
