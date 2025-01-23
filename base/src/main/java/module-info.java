dda7ab36880c2e403739d0b5d929655561590c8b
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-01-22 18:16:37 -0800

    Fix #206: use proper `module-info.java` for building

---
+++ base/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+// JAX-RS Base module-info for Main artifact
+module tools.jackson.jaxrs.base
+{
+    requires com.fasterxml.jackson.annotation;
+    requires tools.jackson.core;
+    requires tools.jackson.databind;
+
+    requires java.ws.rs;
+
+    exports tools.jackson.jaxrs.annotation;
+    exports tools.jackson.jaxrs.base;
+    exports tools.jackson.jaxrs.cfg;
+    exports tools.jackson.jaxrs.util;
+}
